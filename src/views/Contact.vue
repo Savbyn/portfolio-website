09ef47c823ee5562a56d271cb422dfc35f0a8dab
Sabbers <16658822+Savbyn@users.noreply.github.com>
2023-11-27 15:05:36 -0500

    Initial

---
+++ src/views/Contact.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    
+    <h1>My Socials</h1>
+
+    <!-- <div style="margin-bottom:40px;">I'm looking for a job !<br/>Feel free to contact me about any opportunity, or just to chat about cooking, kittens or anything else.</div> -->
+
+    <ul>
+      <li>
+        <i class="fa fa-user-circle fa-lg fa-fw"></i>
+        <a href="http://samyam.dev" target="_blank">https://samyam.dev</a>
+      </li>
+      <li>
+        <i class="fa fa-envelope fa-lg fa-fw"></i>
+        <a href="mailto:sam@samyam.dev" target="_blank">sam@samyam.dev</a>
+      </li>
+      <li>
+        <i class="fab fa-youtube fa-lg fa-fw"></i>
+        <a href="https://youtube.com/@samyam" target="_blank">@samyam</a>
+      </li>
+      <!-- <li>
+        <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
+        <a href="https://www.linkedin.com/me" target="_blank">linkedin.com/me</a>
+      </li> -->
+      <li>
+        <i class="fab fa-twitter fa-lg fa-fw"></i>
+        <a href="https://twitter.com/samyam_youtube" target="_blank">@samyam_youtube</a>
+      </li>
+      <li>
+        <i class="fab fa-instagram fa-lg fa-fw"></i>
+        <a href="https://www.instagram.com/samyam_youtube/" target="_blank">@samyam_youtube</a>
+      </li>
+      <li>
+        <i class="fa fa-gamepad fa-lg fa-fw"></i>
+        <a href="https://sam-yam.itch.io/" target="_blank">sam-yam.itch.io</a>
+      </li>
+      <li>
+        <i class="fab fa-tiktok fa-lg fa-fw"></i>
+        <a href="@_samyam" target="_blank">@_samyam</a>
+      </li>
+      <!-- <li>
+        <i class="fa fa-reddit fa-lg fa-fw"></i>
+        <a href="https://github.com/me" target="_blank">github.com/me</a>
+      </li> -->
+      <li>
+        <i class="fab fa-facebook fa-lg fa-fw"></i>
+        <a href="https://www.facebook.com/samyam.youtube" target="_blank">@samyam.youtube</a>
+      </li>
+
+      <!-- <li>
+        <i class="fa fa-tumblr fa-lg fa-fw"></i>
+        <a href="https://myblog.tumblr.com" target="_blank">myblog.tumblr.com</a>
+      </li> -->
+      <!-- <li>
+        <i class="fa fa-steam fa-lg fa-fw"></i>
+        <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
+      </li> -->
+    </ul>
+
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding-inline-start: 0px;
+}
+
+li {
+    line-height: 1.8em;
+    font-size: 1.2em;
+  }
+
+@media only screen and (min-width: 620px){
+  ul {
+    padding-inline-start: 40px;
+  }
+
+  li {
+    line-height: 2em;
+    font-size: 1.4em;
+  }
+}
+
+i {
+  margin-right: 20px;
+}
+</style>
---
+++ src/views/GameProjects.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Games 🎮</h1>
+
+    <div style="margin-bottom: 30px;">
+      Some stuff I've made.
+    </div>
+
+    <ProjectsList v-bind:projects="projects" />
+
+    <div style="margin-top: 20px;">
+      There is more to see on <a target="_blank" href="https://youtube.com/@samyam">my YouTube</a>.
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ProjectsList from "@/components/ProjectsList.vue";
+import gameProjectsData from "@/data/GameProjectsData.ts";
+
+export default Vue.extend({
+  name: "GameProjects",
+  components: {
+    ProjectsList,
+  },
+  data: function () {
+    return {
+      projects: gameProjectsData,
+    };
+  },
+});
+</script>
